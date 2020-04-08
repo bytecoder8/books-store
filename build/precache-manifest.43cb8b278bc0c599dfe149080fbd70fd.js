28d437c2d65381c36f075e01cdb9f3fe80886a76
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-04-08 15:03:16 +0500

    Add files via upload

---
+++ build/precache-manifest.43cb8b278bc0c599dfe149080fbd70fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbb8c6236b37eb92f9137db78e919cf",
+    "url": "/books-store/build/index.html"
+  },
+  {
+    "revision": "3c629af28a85b11bd36d",
+    "url": "/books-store/build/static/css/main.3a07e278.chunk.css"
+  },
+  {
+    "revision": "2065b258c48c1b8f74cd",
+    "url": "/books-store/build/static/js/2.532d822c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/books-store/build/static/js/2.532d822c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c629af28a85b11bd36d",
+    "url": "/books-store/build/static/js/main.c6abfe16.chunk.js"
+  },
+  {
+    "revision": "1f39b367214e6e987883",
+    "url": "/books-store/build/static/js/runtime-main.88ab242e.js"
+  }
+]);
